1e0fb6867504c2d28fdb7e6b9e821695f68db4de
Hayes Crowley <froglegg@Hayess-MacBook-Pro.local>
2020-02-28 14:43:50 -0500

    configuring for heroku

--- server.js
+++ server.js
@@ -27,10 +27,10 @@ app.get("/api/hello", (req, res) => {
 // the app should proxy requests to server.
 if (process.env.NODE_ENV === "production") {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, "client/dist")));
-  // Handle React routing, return all requests to React app
+  app.use(express.static(path.join(__dirname, "client/dist/client")));
+  // Handle React routing, return all requests to angular app
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "client/dist", "index.html"));
+    res.sendFile(path.join(__dirname, "client/dist/client", "index.html"));
   });
 }
 
