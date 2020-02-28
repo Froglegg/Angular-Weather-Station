1b0de689bfd3aa85901ad65c8ca81aeacf28cb18
Hayes Crowley <froglegg@Hayess-MacBook-Pro.local>
2020-02-28 13:56:46 -0500

    removed test components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,23 +4,18 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HttpClientModule } from "@angular/common/http";
-
-import { TestComponent } from "../app/components/test/test.component";
 import { WeatherViewComponent } from "../app/components/weather-view/weather-view.component";
 import { WeatherDashComponent } from "../app/components/weather-dash/weather-dash.component";
 import { FormComponent } from "../app/components/form/form.component";
 import { TableComponent } from "../app/components/table/table.component";
-import { EmitterTestComponent } from "../app/components/emitter-test/emitter-test.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent,
     WeatherViewComponent,
     WeatherDashComponent,
     FormComponent,
-    TableComponent,
-    EmitterTestComponent
+    TableComponent
   ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
