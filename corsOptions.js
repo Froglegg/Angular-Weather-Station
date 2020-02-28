4fa2f02d371101b455992460c6605de64694f7c5
Hayes Crowley <froglegg@Hayess-MacBook-Pro.local>
2020-02-28 14:53:08 -0500

    added heroku to CORS whitelist

--- corsOptions.js
+++ corsOptions.js
@@ -1,4 +1,8 @@
-const whitelist = ["http://localhost:4200", "http://api.darksky.net"];
+const whitelist = [
+  "http://localhost:4200",
+  "http://api.darksky.net",
+  "https://ng-weather-station.herokuapp.com"
+];
 const corsOptions = {
   origin: function(origin, callback) {
     if (whitelist.indexOf(origin) !== -1 || !origin) {
