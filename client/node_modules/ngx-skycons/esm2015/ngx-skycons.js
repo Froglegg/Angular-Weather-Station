1eb824655cc0c674ad88f50d607cd3abc7d65a93
Hayes Crowley <froglegg@Hayess-MacBook-Pro.local>
2020-04-04 21:04:54 -0400

    yep

--- client/node_modules/ngx-skycons/esm2015/ngx-skycons.js
+++
@@ -1,9 +0,0 @@
-/**
- * @fileoverview added by tsickle
- * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
- */
-/**
- * Generated bundle index. Do not edit.
- */
-export { KEYFRAME, STROKE, TAU, TWO_OVER_SQRT_2, SkyconsComponent, SkyconsTypes, SkyconsModule } from './public_api';
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibmd4LXNreWNvbnMuanMiLCJzb3VyY2VSb290Ijoibmc6Ly9uZ3gtc2t5Y29ucy8iLCJzb3VyY2VzIjpbIm5neC1za3ljb25zLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7QUFJQSxzR0FBYyxjQUFjLENBQUMiLCJzb3VyY2VzQ29udGVudCI6WyIvKipcbiAqIEdlbmVyYXRlZCBidW5kbGUgaW5kZXguIERvIG5vdCBlZGl0LlxuICovXG5cbmV4cG9ydCAqIGZyb20gJy4vcHVibGljX2FwaSc7XG4iXX0=
--- client/node_modules/ngx-skycons/lib/ngx-skycons.module.d.ts
+++
@@ -1,8 +0,0 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './ngx-skycons.component';
-export declare class SkyconsModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<SkyconsModule, [typeof ɵngcc1.SkyconsComponent], never, [typeof ɵngcc1.SkyconsComponent]>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<SkyconsModule>;
-}
-
-//# sourceMappingURL=ngx-skycons.module.d.ts.map
